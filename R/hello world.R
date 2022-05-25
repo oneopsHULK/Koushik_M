d3beb929cdff6bf5f19b5d70210ab0b84b652659
brucebanner2212@gmail.com <brucebanner2212@gmail.com>
2022-05-25 11:29:27 +1000

    try

--- R/hello world.R
+++ R/hello world.R
@@ -1,1 +1,8 @@
 print("hello world")
+URL= "https://raw.githubusercontent.com/markziemann/SLE712_files/master/assessment_task3/bioinfo_asst3_part1_files/gene_expression.tsv"
+NAME= "gene_expression.tsv"
+download.file(URL,destfile=NAME)
+
+URL= "https://raw.githubusercontent.com/markziemann/SLE712_files/master/assessment_task3/bioinfo_asst3_part1_files/growth_data.csv"
+NAME= "growth_data.csv"
+download.file(URL,destfile=NAME)
